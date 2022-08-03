bfba61dc9f9e64d5b2b484a44c83931ba3917bfe
Hugo Moura <hugoeamoura@gmail.com>
2022-08-03 19:10:24 -0300

    primeiro commit

---
+++ etapas.js
@@ -0,0 +1,73 @@
+let etapas = [
+    {
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '666',
+                nome: 'Red Guy',
+                partido: 'HELL',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '999',
+                nome: 'Blue Boy',
+                partido: 'CEU',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Senador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: "PRESIDENTE",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Ciro Gomes',
+                partido: 'PDT',
+                vice: 'Kátia Abreu',
+                fotos:[
+                    {url:'12.png', legenda: 'Presidente'},
+                    {url:'12Vice.png', legenda: "Vice-presidente", small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Lula da Silva',
+                partido: 'PT',
+                vice: 'Geraldo Alckmin',
+                fotos:[
+                    {url:'13.png', legenda: 'Presidente'},
+                    {url:'13Vice.png', legenda: "Vice-presidente", small: true}
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'Simone Tebet',
+                partido: 'MDB',
+                vice: 'Tasso Jereisati',
+                fotos:[
+                    {url:'15.png', legenda: 'Presidente'},
+                    {url:'15Vice.png', legenda: "Vice-presidente", small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Jair Bolsonaro',
+                partido: 'PL',
+                vice: 'Braga Netto',
+                fotos:[
+                    {url:'22.png', legenda: 'Presidente'},
+                    {url:'22Vice.png', legenda: "Vice-presidente", small: true}
+                ]
+            },
+       
+
+        ]
+    }
+    
+];
